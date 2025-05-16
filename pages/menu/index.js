569b6a7590611246acc531fd75af3290dab3f0b5
SiamakMehrnia <siamakme20@gmail.com>
2025-05-16 17:02:48 +0200

    fixmenu

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,5 +1,29 @@
 import MenuLayout from "../../components/menupage/MenuLayout";
 
-export default function Menu() {
-  return <MenuLayout />;
+export async function getServerSideProps() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/menuapi`);
+    const data = await res.json();
+
+    if (!data.success) {
+      throw new Error("Failed to fetch menu items");
+    }
+
+    return {
+      props: {
+        menuItems: data.data,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching menu items:", error.message);
+    return {
+      props: {
+        menuItems: [],
+      },
+    };
+  }
+}
+
+export default function Menu({ menuItems }) {
+  return <MenuLayout menuItems={menuItems} />;
 }
